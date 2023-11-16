ef7c6d39d2ed6b1a4d9b9cc053b4878bf4a1681a
M4tuR <matvei.raud@voco.ee>
2023-11-16 11:29:19 +0200

    Practise2

---
+++ pr2.ps1
@@ -0,0 +1,20 @@
+﻿$Hashtable1=@{
+Name="John", "Joe", "Marry";
+DaysWorked=12, 20 ,18
+}
+$Hashtable2=@{
+Name="John", "Joe", "Marry";
+SalaryPerDay=100, 120, 150
+}
+$johnSalary = $Hashtable1.DaysWorked[0] * $Hashtable2.SalaryPerDay[0]
+$joeSalary = $Hashtable1.DaysWorked[1] * $Hashtable2.SalaryPerDay[1]
+$marySalary = $Hashtable1.DaysWorked[2] * $Hashtable2.SalaryPerDay[2]
+$Hashtable3 = @{ 
+Name=$Hashtable1.Name[0], $Hashtable1.name[1], $Hashtable1.name[2];
+Salary=$johnSalary, $joeSalary, $marySalary}
+
+$Hashtable1
+Write-Host
+$Hashtable2
+Write-Host
+$Hashtable3
