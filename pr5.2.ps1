68d4f8b8720aa3fd7d39c28ff8bb31cd16c5d115
M4tuR <matvei.raud@voco.ee>
2023-11-21 16:00:07 +0200

    Practice 5.2

---
+++ pr5.2.ps1
@@ -0,0 +1,7 @@
+﻿Get-ChildItem -Path "C:\temp\Test"
+$File = Get-ChildItem -Path "C:\temp\Test" | where {$_.Name -like "*.csv"} |Select Name, Lenght
+$sizeinKB = $File.Length/1KB
+$sizeinMB = $File.Length/1MB
+Write-Host "`nFileName : "$File.Name
+Write-Host "Size in KB : "$sizeinKB
+Write-Host "Size in MB : "$sizeinMB
