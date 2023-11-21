03345cd2b1eb32093f43156165aa2f51bf5c9090
M4tuR <matvei.raud@voco.ee>
2023-11-21 17:43:04 +0200

    Practice 12.2

---
+++ pr12.2.ps1
@@ -0,0 +1,19 @@
+﻿function countservice
+{
+param
+(
+[string]$status
+)
+if($status -eq "Running")
+{
+$Start = Get-Service |?{$_.Status -eq "Running"}
+Write-Host "Total Services in running state = "$start.count
+}
+elseif($status -eq "Stopped")
+{
+$stop = Get-Service | ?{$_.Status -eq "Stopped"}
+Write-Host "Total Services in stopped state = "$stop.count
+}
+}
+countservice -status Running
+countservice -status Stopped
